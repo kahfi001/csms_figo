430e33f48f36cad5666d888f81c6d35ff4ca0835
bagas12dwi <bag12dwi.s@gmail.com>
2024-06-07 01:58:41 +0700

    change backgorund styling in login

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -18,7 +18,7 @@
 
 <body class="font-sans text-gray-900 antialiased">
     <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100"
-        style="background-image: {{ URL::asset('sb-admin/img/bg-auth.jpg') }}; background-size:cover;">
+        style="background-image: url('{{ URL::asset('sb-admin/img/bg-auth.jpg') }}'); background-size:cover;">
         <div>
             <a href="/">
                 <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
